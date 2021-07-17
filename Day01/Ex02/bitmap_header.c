e7a16eca091a84f38e39e444896735008bbf4299
Alexis <alexis.martin@epitech.eu>
2021-07-17 22:35:47 +0200

    [ADD] Ex02 of Day01

---
+++ Day01/Ex02/bitmap_header.c
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2021
+** CPPool_Epitech_Tech2
+** File description:
+** bitmap_header
+*/
+
+#include "bitmap.h"
+
+void make_bmp_header(bmp_header_t * header, size_t size)
+{
+    header->magic = 0x424D;
+    header->size = (size * size * 4) + (sizeof(bmp_header_t)) +
+    (sizeof(bmp_info_header_t));
+    header->_app1 = 0;
+    header->_app2 = 0;
+    header->offset = sizeof(bmp_header_t) + sizeof(bmp_info_header_t);
+}
+
+void make_bmp_info_header(bmp_info_header_t * header, size_t size)
+{
+    header->size = sizeof(bmp_info_header_t);;
+    header->width = size;
+    header->height = size;
+    header->planes = 1;
+    header->bpp = 32;
+    header->compression = 0;
+    header->raw_data_size = size * size * 4;
+    header->h_resolution = 0;
+    header->v_resolution = 0;
+    header->palette_size = 0;
+    header->important_colors = 0;
+}
